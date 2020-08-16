784ff8c53b8b49251e3b6bba5a6da0663becadeb
logshoy <logshoy@gmail.com>
2020-08-16 18:54:28 +0500

    Updates

---
+++ precache-manifest.07aaba10d24135433f55c90c2a7e60f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c534145f779d168e1cb86e8d8074023",
+    "url": "/sberbank-sticker/index.html"
+  },
+  {
+    "revision": "e991dea71e400aa1b2ba",
+    "url": "/sberbank-sticker/static/css/main.69a48f85.chunk.css"
+  },
+  {
+    "revision": "a818e9611491e751805c",
+    "url": "/sberbank-sticker/static/js/2.edc72b54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sberbank-sticker/static/js/2.edc72b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e991dea71e400aa1b2ba",
+    "url": "/sberbank-sticker/static/js/main.f318a21d.chunk.js"
+  },
+  {
+    "revision": "21aeef8f99d783e99395",
+    "url": "/sberbank-sticker/static/js/runtime-main.e26ce6a9.js"
+  }
+]);
